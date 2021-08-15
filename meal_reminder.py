910e9ac81911697cc30020e5af7fbb4f7a6d7cb3
gerpang <geraldinepyh@gmail.com>
2021-08-15 15:42:35 +0800

    added meal_reminder

--- meal_reminder.py
+++ meal_reminder.py
@@ -1,11 +1,13 @@
+import argparse
 from decouple import config
 from telegram.client import Telegram
-from datetime import datetime
 
-"""Checks whether the bot is still alive
-"""
+
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("meal", help="Message text")
+    args = parser.parse_args()
 
     tg = Telegram(
     api_id= config('TELEGRAM_API_ID'),
@@ -13,19 +15,28 @@ if __name__ == "__main__":
     phone = config('PHONE'),
     database_encryption_key= config('DATABASE_ENCRYPTION_KEY')
     )
+    # you must call login method before others
     tg.login()
+
+    # if this is the first run, library needs to preload all chats
+    # otherwise the message will not be sent
     result = tg.get_chats()
+
+    # `tdlib` is asynchronous, so `python-telegram` always returns you an `AsyncResult` object.
+    # You can wait for a result with the blocking `wait` method.
     result.wait()
 
     if result.error:
         print(f"get chats error: {result.error_info}")
     else:
         print(f"chats: {result.update}")
-    
-    text="[🤖] It's {}.".format(datetime.now().strftime("%H:%M"))
+
+    text = """
+    [🤖] THIS IS A REMINDER. PLEASE EAT YOUR {}. 
+    """.format(args.meal.upper())
 
     result = tg.send_message(
-        chat_id=int(config('USER_ID_ME')),  
+        chat_id=int(config('USER_ID_BF')),  # args.chat_id,
         text=text,
     )
 
